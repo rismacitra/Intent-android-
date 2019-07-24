3142e26239b5db50a35f2a498d27c6f967019f7b
rismacitra <rismacitradewi16@gmail.com>
2019-07-24 12:37:15 +0700

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.dummy.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener (this);
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_move_activity;
+                Intent moveIntent =new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+        }
+    }
+}
